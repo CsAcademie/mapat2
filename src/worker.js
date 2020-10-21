a336fb2ead3d90e6eea811f22548042ec086712a
Stéphane Codazzi <stephane@codazzi.fr>
2020-10-21 19:01:55 +0000

    Bump version to 1.2.0

--- src/worker.js
+++ src/worker.js
@@ -1,16 +1,15 @@
-let Worker = function () {
-    return;
-};
+let Worker = function () {};
 
 Worker.prototype = {
     startButton: null,
     downloadPath: null,
     mapsToCheck: [],
     mapsToDownload: [],
-    mapsToUntar: [],
+    mapsToUnTar: [],
     downloading: false,
     mapUrl: null,
     logs: [],
+    apiUrl: 'https://portal.csacademie.fr/api/maps',
 
     init: function() {
         this.startButton = document.getElementById('startSync');
@@ -18,10 +17,9 @@ Worker.prototype = {
         this.folderButton = document.getElementById('downloadPathButton');
         this.startButton.addEventListener('click', window.appWorker.launchSync);
         this.stopButton.addEventListener('click', window.appWorker.stopSync);
-        this.folderButton.addEventListener('click', window.appWorker.openModalPathFile);
+        this.folderButton.addEventListener('click', window.appWorker.chooseMapFolder);
 
-        let versionId = window.require('electron').remote.app.getVersion();
-        document.getElementById('versionId').innerHTML = versionId;
+        document.getElementById('versionId').innerHTML = window.mapat.getVersion();
 
         window.appWorker.downloadPath = window.localStorage.getItem("downloadPath");
         window.appWorker.mapUrl = 'https://dl.csacademie.fr/maps';
@@ -33,38 +31,38 @@ Worker.prototype = {
     launchSync() {
         if (window.appWorker.downloadPath === undefined || window.appWorker.downloadPath === null
             || window.appWorker.downloadPath === '' || window.appWorker.downloadPath.length < 2) {
+            window.appWorker.addLog('ERROR', 'Mauvaise destination pour télécharger les cartes');
             alert('Vous devez choisir un chemin valide pour télécharger les cartes.');
             return;
         }
-        window.appWorker.addLog("Start synchronization");
+
+        window.appWorker.addLog('INFO', 'Début de la vérification des cartes');
         window.appWorker.startButton.disabled = true;
         window.appWorker.stopButton.disabled = false;
         window.appWorker.getMapList(1);
     },
 
     stopSync() {
-        window.appWorker.addLog("Stop synchronization");
+        window.appWorker.addLog('INFO', 'Arrêt de la vérification');
         window.appWorker.startButton.disabled = false;
         window.appWorker.stopButton.disabled = true;
         window.appWorker.mapsToCheck = [];
         window.appWorker.mapsToDownload = [];
-        window.appWorker.mapsToUntar = [];
+        window.appWorker.mapsToUnTar = [];
     },
 
-    openModalPathFile() {
-        const { dialog } = require('electron').remote
-        let options = {properties:["openDirectory"]}
-        let dir = dialog.showOpenDialogSync(options)
-        window.localStorage.setItem("downloadPath", dir[0]);
-        window.appWorker.downloadPath = dir[0];
-        document.getElementById('downloadPath').innerHTML = dir[0];
+    chooseMapFolder() {
+        let folder = window.mapat.openModalPathFile();
+
+        window.localStorage.setItem("downloadPath", folder[0]);
+        window.appWorker.downloadPath = folder[0];
+        document.getElementById('downloadPath').innerHTML = folder[0];
+        window.appWorker.addLog('INFO', 'Les cartes seront téléchargées dans : ' + folder[0])
     },
 
     getMapList(page) {
-        const apiUrl = 'https://portal.csacademie.fr/api/maps';
-
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+        let xHttp = new XMLHttpRequest();
+        xHttp.onreadystatechange = function() {
             if (this.readyState === 4 && this.status === 200) {
                 let response = JSON.parse(this.responseText);
                 let mapList = response['hydra:member']
@@ -74,109 +72,91 @@ Worker.prototype = {
                         window.appWorker.mapsToCheck.push(mapList[i]);
                     }
                 }
+
                 if (response['hydra:view'] && response['hydra:view']['hydra:next']) {
                     window.appWorker.getMapList(page + 1)
                 } else {
-                    window.appWorker.addLog("Map list OK, check it");
+                    window.appWorker.addLog('INFO', 'Map list OK, check it');
                 }
             }
         };
-        xhttp.open('GET', apiUrl + '?page=' + page + '&itemsPerPage=50&isDeleted=false&_order[id]=DESC', true);
-        xhttp.send();
+        xHttp.open(
+          'GET',
+          window.appWorker.apiUrl + '?page=' + page + '&itemsPerPage=50&isDeleted=false&_order[id]=DESC',
+          true
+        );
+        xHttp.send();
     },
 
     checkMaps() {
-        const fs = require('fs');
-        const md5File = require('md5-file');
-
-        if (window.appWorker.mapsToCheck.length > 0) {
-            let map = window.appWorker.mapsToCheck[0];
-
-            document.getElementById('check_max').innerHTML = (window.appWorker.mapsToCheck.length - 1).toString();
-            document.getElementById('check_current_map').innerHTML = map.name;
-
-            window.appWorker.mapsToCheck.splice(0, 1);
-            let filePath = window.appWorker.downloadPath + '\\' + map.name; // @TODO LINUX/MAC
-            if (fs.existsSync(filePath)) {
-                const hash = md5File.sync(filePath);
-                if (hash !== map.md5) {
-                    fs.unlinkSync(filePath);
-                    window.appWorker.addLog("Hash error: " + map.name);
-                    window.appWorker.mapsToDownload.push(map);
-                }
-            } else {
-                window.appWorker.mapsToDownload.push(map);
-                document.getElementById('download_max').innerHTML = (window.appWorker.mapsToDownload.length - 1).toString();
-            }
-            setTimeout(window.appWorker.checkMaps, 5);
-        } else {
+        if (window.appWorker.mapsToCheck.length === 0) {
             setTimeout(window.appWorker.checkMaps, 1000);
+            return;
         }
+
+        let map = window.appWorker.mapsToCheck[0];
+        let filePath = window.appWorker.downloadPath + window.mapat.getSystemPathSeparator() + map.name;
+        window.appWorker.mapsToCheck.splice(0, 1);
+        document.getElementById('check_max').innerHTML = window.appWorker.mapsToCheck.length.toString();
+        document.getElementById('check_current_map').innerHTML = map.name;
+
+        if (!window.mapat.isFileExist(filePath)) {
+            window.appWorker.mapsToDownload.push(map);
+        } else if (window.mapat.getFileMd5(filePath) !== map.md5) {
+            window.mapat.deleteFile(filePath)
+            window.appWorker.addLog('INFO', 'Mise à jour de la carte ' + map.name);
+            window.appWorker.mapsToDownload.push(map);
+        }
+
+        document.getElementById('download_max').innerHTML = window.appWorker.mapsToDownload.length.toString();
+        setTimeout(window.appWorker.checkMaps, 5);
     },
 
     downloadMap() {
-        if (!window.appWorker.downloading && window.appWorker.mapsToDownload.length > 0) {
-            let map = window.appWorker.mapsToDownload[0];
-            window.appWorker.downloading = true;
-
-            document.getElementById('download_current_map').innerHTML = map.name;
-            document.getElementById('download_max').innerHTML = (window.appWorker.mapsToDownload.length - 1).toString();
-
-            window.appWorker.mapsToDownload.splice(0, 1);
-            let filePath = window.appWorker.downloadPath + '\\' + map.name; // @TODO LINUX/MAC
-            // if (map.size > 150000000) {
-            //     // bz2
-            // } else {
-            //     // zip
-            // }
-            let downloadUrl = window.appWorker.mapUrl + '/' + map.name;
-
-            window.appWorker.downloadFile(downloadUrl, filePath);
+        if (window.appWorker.downloading) {
+            setTimeout(window.appWorker.downloadMap, 1000);
+            return
         }
+
+        if (window.appWorker.mapsToDownload.length === 0) {
+            document.getElementById('download_current_map').innerHTML = '';
+            document.getElementById('download_max').innerHTML = window.appWorker.mapsToDownload.length.toString();
+            setTimeout(window.appWorker.downloadMap, 1000);
+            return
+        }
+
+        let map = window.appWorker.mapsToDownload[0];
+        window.appWorker.mapsToDownload.splice(0, 1);
+        window.appWorker.downloading = true;
+        document.getElementById('download_current_map').innerHTML = map.name;
+        document.getElementById('download_max').innerHTML = window.appWorker.mapsToDownload.length.toString();
+
+        let filePath = window.appWorker.downloadPath + window.mapat.getSystemPathSeparator() + map.name;
+        // if (map.size > 150000000) {
+        //     // bz2
+        // } else {
+        //     // zip
+        // }
+        let fileUrl = window.appWorker.mapUrl + '/' + map.name;
+
+        let progressCallback = (percent) => {
+            document.getElementById('dl_progress_bar').style.width = percent + '%'
+        }
+        let endCallback = () => {
+            window.appWorker.downloading = false;
+            document.getElementById('download_current_map').innerHTML = '';
+        }
+        window.mapat.downloadFile(fileUrl, filePath, progressCallback, endCallback)
+
         setTimeout(window.appWorker.downloadMap, 1000);
     },
 
-    downloadFile: function(file_url , targetPath) {
-        const request = require('request');
-        const fs = require('fs');
-
-        let received_bytes = 0;
-        let total_bytes = 0;
-
-        let req = request({
-            method: 'GET',
-            uri: file_url
-        });
-
-        let out = fs.createWriteStream(targetPath);
-        req.pipe(out);
-
-        req.on('response', function ( data ) {
-            // Change the total bytes value to get progress later.
-            total_bytes = parseInt(data.headers['content-length' ]);
-        });
-
-        req.on('data', function(chunk) {
-            received_bytes += chunk.length;
-            window.appWorker.showProgress(received_bytes, total_bytes);
-        });
-
-        req.on('end', function() {
-            window.appWorker.downloading = false;
-            document.getElementById('download_current_map').innerHTML = '';
-        });
-    },
-
-    showProgress: function(received,total) {
-        let percentage = (received * 100) / total;
-        document.getElementById('dl_progress_bar').style.width = percentage + '%'
-    },
-
-    addLog(message) {
+    addLog(type, message) {
         console.log(message);
-        window.appWorker.logs.push(message);
+        window.appWorker.logs.push('[' + type + ']: ' + message);
         document.getElementById('logs').innerHTML = window.appWorker.logs.join('<br />');
     }
 };
+
 window.appWorker = new Worker();
 window.appWorker.init();
